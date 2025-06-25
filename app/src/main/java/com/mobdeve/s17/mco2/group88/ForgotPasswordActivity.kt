b614aa9250cc24ddb2aeb504bdf0029a55f27f99
LianneMax <165748673+LianneMax@users.noreply.github.com>
2025-06-25 23:11:17 +0800

    fixed eye on sign in page

---
+++ app/src/main/java/com/mobdeve/s17/mco2/group88/ForgotPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s17.mco2.group88
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_forgotpassword)
+    }
+}
